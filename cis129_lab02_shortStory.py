224ff643cbd255203b0b5415c873800247969463
dpopov75 <160723862+dpopov75@users.noreply.github.com>
2024-02-21 22:19:25 -0700

    Replaced "Hello World" w/ a short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+# Author: Daniel Popov
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print('Once upon a time')
+print('There was a CIS129 student')
+print('And he had to write a short creative story')
+print('This is that story :)')
